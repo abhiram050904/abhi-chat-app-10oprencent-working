087fc2e09a163a9f7af7009cb24f834a60f429b0
abhiram050904 <abhiram050904@gmail.com>
2024-07-31 22:24:40 +0530

    first commit

---
+++ public/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SetAvatar from "./pages/setAvatar";
+import Chat from "./pages/chat";
+import Login from "./pages/login";
+import Register from "./pages/register";
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/setAvatar" element={<SetAvatar />} />
+        <Route path="/" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
