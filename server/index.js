d2533081146eedbb961786a65f646e744f3717cc
ABHIRAM GATTAMNENI <142649536+abhiram050904@users.noreply.github.com>
2025-09-13 17:33:22 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ const server = app.listen(process.env.PORT || 5000, () =>
 
 const io = socket(server, {
   cors: {
-    origin: "*",  // allow from everywhere
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
     credentials: true,
   },
 });
